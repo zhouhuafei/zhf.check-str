3ff49966f2cb34b3734b79d8dc1d299126752ae9
zhouhuafei <1123486116@qq.com>
2021-12-09 12:41:24 +0800

    fix: 🐛 修复ReferenceError: primordials is not defined报错

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@ const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 
 gulp.task('watch', function () {
-    gulp.watch(['src/**/*.**'], ['es5', 'jsmin']);
+    return gulp.watch(['src/**/*.**'], gulp.series('es5', 'jsmin'));
 });
 
 gulp.task('es5', function () {
-    gulp.src('src/**/*.js').pipe(babel()).pipe(gulp.dest('dist'));
+    return gulp.src('src/**/*.js').pipe(babel()).pipe(gulp.dest('dist'));
 });
 
 gulp.task('jsmin', function () {
-    gulp.src('src/**/*.js').pipe(babel()).pipe(uglify()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('dist'));
+    return gulp.src('src/**/*.js').pipe(babel()).pipe(uglify()).pipe(rename({ suffix: '.min' })).pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['es5', 'jsmin', 'watch']);
+gulp.task('default', gulp.series('es5', 'jsmin', 'watch'));
